4e76a25f8ef573ca7374f947ee39a71d0930485b
sridhar143s <95617034+sridhar143s@users.noreply.github.com>
2022-09-29 21:36:28 +0530

    Add files via upload

---
+++ sridhar.py
@@ -0,0 +1,41 @@
+from google.colab import drive
+drive.mount('/content/drive')
+from sklearn import datasets
+iris= datasets.load_iris()
+print(iris.target_names)
+print(iris.feature_names)
+print(iris.data[0:5])
+
+
+print(iris.target)
+import pandas as pd
+data=pd.DataFrame({
+    'sepal length':iris.data[:,0],
+    'sepal width':iris.data[:,1],
+    'petal length':iris.data[:,2],
+    'petal width':iris.data[:,3],
+    'species':iris.target
+})
+data.head()
+
+from sklearn.model_selection import train_test_split
+
+X=data[['sepal length', 'sepal width', 'petal length', 'petal width']] 
+y=data['species']  
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+clf=RandomForestClassifier(n_estimators=85)
+
+
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+from sklearn import metrics
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
